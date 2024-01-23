41a52442d737172a9db1e0d5599c00371a634b3d
willbarbos4 <157074225+willbarbos4@users.noreply.github.com>
2024-01-22 23:24:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var elementosDuvidas = document.querySelectorAll('.duvida')
+
+elementosDuvidas.forEach(function (duvida) {
+duvida.addEventListener('click',function () {
+    duvida.classList.toggle('ativa')
+})
+})
